6bcc9e135563dc8cd640066d3ea81fd240f2f882
Pranav-J-Kumar-9894 <114971590+Pranav-J-Kumar-9894@users.noreply.github.com>
2022-10-06 16:20:47 +0530

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-fetch('https://blr1.blynk.cloud/external/api/isHardwareConnected?token=APuw3xTam8hTBHUTbSpt-c36Hf2HO2x0')
-.then((data)=>{
-  return data.json();
-}).then((stat)=>{
-  console.log(stat);
-  document.getElementById('value').innerHTML = stat ;
-})
